088f3a53d0f26c9c84ba5074327166e0925a5125
Joe Prathap P J <joeprathappj@gmail.com>
2024-06-09 12:20:08 +0530

    refactor: optimization

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.gridlayer.googlemutant';
@@ -14,21 +14,22 @@ const CarIcon = L.icon({
   popupAnchor: [0, -19]
 });
 
+function RecenterMap({ lat, lng }) {
+  const map = useMap();
+  useEffect(() => {
+    map.flyTo([lat, lng], map.getZoom(), {
+      animate: true,
+      duration: 1.5 // Adjust the duration for smoother animation
+    });
+  }, [lat, lng, map]);
+  return null;
+}
+
 function Home() {
   const [cars, setCars] = useState([]);
-  const [mapCenter, setMapCenter] = useState([52.07134812014466,-1.015803606943344]);
+  const [mapCenter, setMapCenter] = useState([52.07134812014466, -1.015803606943344]);
   const [sosMessages, setSosMessages] = useState([]);
-
-  function RecenterMap({ lat, lng }) {
-    const map = useMap();
-    useEffect(() => {
-      map.flyTo([lat, lng], map.getZoom(), {
-        animate: true,
-        duration: 1.5 // Adjust the duration for smoother animation
-      });
-    }, [lat, lng, map]);
-    return null;
-  }
+  const [carPaths, setCarPaths] = useState({});
 
   useEffect(() => {
     const socket = io('http://localhost:5000', {
@@ -39,7 +40,18 @@ function Home() {
       setCars((prevCars) => {
         const updatedCars = prevCars.filter(car => car.carId !== data.carId);
         updatedCars.push(data);
-        // console.log(cars)
+
+        // Update car paths
+        setCarPaths((prevPaths) => {
+          const newPaths = { ...prevPaths };
+          if (!newPaths[data.carId]) {
+            newPaths[data.carId] = [];
+          }
+          newPaths[data.carId].push([data.latitude, data.longitude]);
+          console.log('Updated Paths:', newPaths); // Debugging line
+          return newPaths;
+        });
+
         return updatedCars;
       });
     });
@@ -66,55 +78,56 @@ function Home() {
         </div>
       </header>
       <div className='flex flex-row flex-grow'>
-
-      <div className='flex w-[20%] bg-gray-200 shadow-md rounded-r-md'>
-        <div className='flex flex-col p-4'>
-          <h1 className='text-xl font-bold'>Cars</h1>
-          {cars.map((car) => (
-            <div key={car.carId} className='flex flex-col mt-2'>
-              <span className='font-bold'>Car ID: {car.carId}</span>
-              <span>Latitude: {car.latitude}</span>
-              <span>Longitude: {car.longitude}</span>
-            </div>
-          ))}
+        <div className='flex w-[20%] bg-gray-200 shadow-md rounded-r-md'>
+          <div className='flex flex-col p-4'>
+            <h1 className='text-xl font-bold'>Cars</h1>
+            {cars.map((car) => (
+              <div key={car.carId} className='flex flex-col mt-2'>
+                <span className='font-bold'>Car ID: {car.carId}</span>
+                <span>Latitude: {car.latitude}</span>
+                <span>Longitude: {car.longitude}</span>
+              </div>
+            ))}
+          </div>
         </div>
+        <main className="flex flex-grow ">
+          {cars && cars.length > 0 && (
+            <MapContainer className="flex flex-grow h-[100%]" center={mapCenter} zoom={15} style={{ width: "100%" }}>
+              <TileLayer
+                url="http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+                attribution='&copy; <a href="https://maps.google.com">Google Maps</a>'
+                subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
+              />
+              {cars.map((car) => (
+                <Marker key={car.carId} position={[car.latitude, car.longitude]} icon={CarIcon}>
+                  <Popup>
+                    Car ID: {car.carId}<br />
+                    Latitude: {car.latitude}<br />
+                    Longitude: {car.longitude}
+                  </Popup>
+                </Marker>
+              ))}
+              {Object.keys(carPaths).map(carId => (
+                <Polyline key={carId} positions={carPaths[carId]} color="blue" />
+              ))}
+              <Polyline key={1} positions={carPaths['Car-1']} color="red" />
+              {cars.length > 0 && (
+                <RecenterMap lat={cars[cars.length - 1].latitude} lng={cars[cars.length - 1].longitude} />
+              )}
+            </MapContainer>
+          )}
+          {sosMessages.length > 0 && (
+            <div className="relative px-4 py-3 text-red-700 bg-red-100 border border-red-400 rounded" role="alert">
+              <strong className="font-bold">SOS Alerts:</strong>
+              <ul className="pl-5 mt-2 list-disc">
+                {sosMessages.map((msg, index) => (
+                  <li key={index}>{msg.carId}: {msg.message}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </main>
       </div>
-
-      <main className="flex flex-grow ">
-       
-      
-      {cars && cars.length > 0 && (
-         <MapContainer className="flex flex-grow h-[100%]" center={mapCenter} zoom={15} style={{ width: "100%" }}>
-         <TileLayer
-           url="http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
-           attribution='&copy; <a href="https://maps.google.com">Google Maps</a>'
-           subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
-         />
-         {cars.map((car) => (
-           <Marker key={car.carId} position={[car.latitude, car.longitude]} icon={CarIcon}>
-             <Popup>
-               Car ID: {car.carId}<br />
-               Latitude: {car.latitude}<br />
-               Longitude: {car.longitude}
-             </Popup>
-           </Marker>
-         ))}
-         <RecenterMap lat={cars[cars.length - 1].latitude} lng={cars[cars.length - 1].longitude} />
-       </MapContainer>
-      )}
-       {sosMessages.length > 0 && (
-         <div className="relative px-4 py-3 text-red-700 bg-red-100 border border-red-400 rounded" role="alert">
-           <strong className="font-bold">SOS Alerts:</strong>
-           <ul className="pl-5 mt-2 list-disc">
-             {sosMessages.map((msg, index) => (
-               <li key={index}>{msg.carId}: {msg.message}</li>
-             ))}
-           </ul>
-         </div>
-       )}
-     </main>
-      </div>
-    
     </div>
   );
 }
